b5adfc1d395878849503dd61b9facbf18801598b
alfajkhilji27-source <alfajkhilji27@gmail.com>
2026-07-31 10:13:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("AK Ventures v1.1 Loaded Successfully");
+window.addEventListener("load",()=>{
+
+document.body.style.opacity="1";
+
+});
+
+console.log("AK Ventures Loaded Successfully");
