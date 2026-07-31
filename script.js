b7b7877d80505b9be1eeb8e865c91a1652ac0bae
alfajkhilji27-source <alfajkhilji27@gmail.com>
2026-07-31 20:30:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -55,3 +55,16 @@ window.addEventListener("scroll",()=>{
 topBtn.onclick=()=>window.scrollTo({top:0,behavior:"smooth"});
 
 });
+/* Scroll Progress */
+
+window.addEventListener("scroll",()=>{
+
+const scrollTop=document.documentElement.scrollTop;
+
+const height=document.documentElement.scrollHeight-document.documentElement.clientHeight;
+
+const progress=(scrollTop/height)*100;
+
+document.getElementById("progress-bar").style.width=progress+"%";
+
+});
