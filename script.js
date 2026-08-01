009f8cce9c6fa30fbe6189a562b9dcbf3a558023
alfajkhilji27-source <alfajkhilji27@gmail.com>
2026-08-01 15:55:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -108,3 +108,30 @@ menu.innerHTML="☰";
 }
 
 });
+/* ==========================================================
+                    SCROLL REVEAL
+========================================================== */
+
+const reveals=document.querySelectorAll(".reveal");
+
+function revealSections(){
+
+reveals.forEach(section=>{
+
+const top=section.getBoundingClientRect().top;
+
+const visible=window.innerHeight-120;
+
+if(top<visible){
+
+section.classList.add("active");
+
+}
+
+});
+
+}
+
+window.addEventListener("scroll",revealSections);
+
+revealSections();
