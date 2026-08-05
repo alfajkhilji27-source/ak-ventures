4c7e019b9ed5bc2fab92cdc7a460ed84a6ee13fd
alfajkhilji27-source <alfajkhilji27@gmail.com>
2026-08-06 00:07:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -441,3 +441,21 @@ window.addEventListener("load", () => {
     }, 2800);
 
 });
+/* Premium Cursor */
+
+const dot = document.querySelector(".cursor-dot");
+const outline = document.querySelector(".cursor-outline");
+
+window.addEventListener("mousemove",(e)=>{
+
+if(!dot || !outline) return;
+
+dot.style.left=e.clientX+"px";
+dot.style.top=e.clientY+"px";
+
+outline.style.left=e.clientX+"px";
+outline.style.top=e.clientY+"px";
+
+});
+
+
